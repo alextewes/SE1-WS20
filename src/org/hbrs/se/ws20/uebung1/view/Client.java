a328b141ac77a2266e301fd715f76405cab91c7c
Alexander Tewes <alexandertewes7@gmail.com>
2020-11-04 14:52:11 +0100

    Implemented display method

--- src/org/hbrs/se/ws20/uebung1/view/Client.java
+++ src/org/hbrs/se/ws20/uebung1/view/Client.java
@@ -1,18 +1,22 @@
 package org.hbrs.se.ws20.uebung1.view;
 
+import org.hbrs.se.ws20.uebung1.control.Factory;
+import org.hbrs.se.ws20.uebung1.control.Translator;
+
 class Client {
 
 	/*
 	 * Methode zur Ausgabe einer Zahl auf der Console
 	 */
 	public void display( int aNumber ){
-		// In dieser Methode soll die Methode translateNumber 
-		// mit dem übergegebenen Wert der Variable aNumber 
+		// In dieser Methode soll die Methode translateNumber
+		// mit dem übergegebenen Wert der Variable aNumber
 		// aufgerufen werden.
 		// Strenge Implementierung gegen das Interface Translator gewuenscht!
-
+		Factory f = new Factory();
+		Translator gt = f.createTranslator();
 		System.out.println("Das Ergebnis der Berechnung: " +
-				"[das Ergebnis an dieser Stelle]" );
+				gt.translateNumber(aNumber) );
 
 	}
 }
