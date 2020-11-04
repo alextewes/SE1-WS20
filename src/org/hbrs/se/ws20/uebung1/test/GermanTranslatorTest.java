8e5b85f71dcedb77fd7d4066a38ecacdbbf3026e
Alexander Tewes <alexandertewes7@gmail.com>
2020-11-04 23:06:59 +0100

    Added test cases

--- src/org/hbrs/se/ws20/uebung1/test/GermanTranslatorTest.java
+++ src/org/hbrs/se/ws20/uebung1/test/GermanTranslatorTest.java
@@ -2,7 +2,8 @@ package org.hbrs.se.ws20.uebung1.test;
 
 import org.hbrs.se.ws20.uebung1.control.GermanTranslator;
 import org.hbrs.se.ws20.uebung1.control.Translator;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class GermanTranslatorTest {
 
@@ -10,6 +11,10 @@ class GermanTranslatorTest {
     void translateNumberTest() {
         Translator t = new GermanTranslator();
         assertEquals("zwei", t.translateNumber(2));
+        assertThrows(IllegalArgumentException.class, () -> t.translateNumber(-1));
+        assertThrows(IllegalArgumentException.class, () -> t.translateNumber(16));
+        assertThrows(IllegalArgumentException.class, () -> t.translateNumber(0));
+
     }
 
 }
