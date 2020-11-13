4a1e47b50655def0ae96d35fe10e7f3b6963ffaf
Alexander Tewes <alexandertewes7@gmail.com>
2020-11-13 15:34:30 +0100

    Klasse Container und Methode addMember entwickelt

---
+++ src/org/hbrs/se/ws20/uebung2/Container.java
@@ -0,0 +1,19 @@
+package org.hbrs.se.ws20.uebung2;
+
+import java.util.LinkedList;
+
+public class Container {
+
+    LinkedList<Member> memberLinkedList;
+
+    public void addMember(Member member) throws ContainerExcpetion {
+
+        boolean b = this.memberLinkedList.add(member);
+        if(!b) {
+            throw new ContainerExcpetion("Das Member-Objekt mit der ID " +
+                                         member.getID() + " ist bereits vorhanden!");
+        }
+
+    }
+
+}
