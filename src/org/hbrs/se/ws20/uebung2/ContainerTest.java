b1558336d07bcd12b4a8790696fbad643641ae17
Alexander Tewes <alexandertewes7@gmail.com>
2020-11-13 15:41:57 +0100

    Testklasse für Container generiert

---
+++ src/org/hbrs/se/ws20/uebung2/ContainerTest.java
@@ -0,0 +1,20 @@
+package org.hbrs.se.ws20.uebung2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerTest {
+
+    @Test
+    void addMember() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+
+    @Test
+    void size() {
+    }
+}
