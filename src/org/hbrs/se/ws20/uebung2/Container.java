2f4e85792ff229d73e6396fa291f7255d54c4516
Alexander Tewes <alexandertewes7@gmail.com>
2020-11-16 19:21:05 +0100

    Kommentare hinzugefügt und Fehler in deleteMember behoben

--- src/org/hbrs/se/ws20/uebung2/Container.java
+++ src/org/hbrs/se/ws20/uebung2/Container.java
@@ -4,39 +4,50 @@ import java.util.LinkedList;
 
 public class Container {
 
-    LinkedList<Member> memberLinkedList;
+    private final LinkedList<Member> memberList;
 
+    public Container() {
+        memberList = new LinkedList<>();
+    }
+
+
+    // Methode zum Hinzufügen von Member-Objekten zum Container
     public void addMember(Member member) throws ContainerException {
-
-        boolean b = this.memberLinkedList.add(member);
-        if(!b) {
+        if(memberList.contains(member)) {
             throw new ContainerException("Das Member-Objekt mit der ID " +
-                                         member.getID() + " ist bereits vorhanden!");
+                    member.getID() + " ist bereits vorhanden!");
         }
+        memberList.add(member);
+
 
     }
 
+    // Methode zum Löschen von Member-Objekten des Containers
     public String deleteMember(Integer id) {
-        Integer memberID = null;
-        for(Member member : memberLinkedList) {
+        Member deletedMember = null;
+        for (Member member : memberList) {
             if(member.getID().equals(id)) {
-                memberID = member.getID();
-                memberLinkedList.remove(member);
+                deletedMember = member;
+                memberList.remove(member);
             }
-
         }
-        return memberID == null ? "Fehler: Objekt nicht gefunden." :
-                "Objekt: " + memberID + " gelöscht.";
+        return deletedMember == null ? "Fehler: Objekt nicht gefunden." :
+                "Objekt: " + deletedMember.getID() + " gelöscht.";
     }
 
+    // Methode zur Ausgabe der Member des Containers auf Kommandozeile
     public void dump() {
-        for(Member member : memberLinkedList) {
-            System.out.println(member.toString() + " (ID = " + member.getID() + ")");
+        for(Member member : memberList) {
+            System.out.println(member.toString());
         }
     }
 
     public int size() {
-        return memberLinkedList.size();
+        return memberList.size();
+    }
+
+    public Member get(int i) {
+        return memberList.get(i);
     }
 
 }
