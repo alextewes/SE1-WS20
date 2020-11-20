8466a7b80061c9ea33aab5a56a6c853ea936b32b
Alexander Tewes <alexandertewes7@gmail.com>
2020-11-20 18:22:45 +0100

    Refactor

---
+++ src/org/hbrs/se/ws20/uebung3/control/ContainerException.java
@@ -0,0 +1,9 @@
+package org.hbrs.se.ws20.uebung3.control;
+
+public class ContainerException extends Exception {
+
+    public ContainerException(String s) {
+        super(s);
+    }
+
+}
---
+++ src/org/hbrs/se/ws20/uebung3/view/MemberView.java
@@ -0,0 +1,4 @@
+package org.hbrs.se.ws20.uebung3.view;
+
+public class MemberView {
+}
