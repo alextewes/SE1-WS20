4cbdceb1e55e9a601a9bb0d5af88b69b905e23c1
Alexander Tewes <alexandertewes7@gmail.com>
2020-11-21 11:58:49 +0100

    Methode dump in view Klasse verschoben

--- src/org/hbrs/se/ws20/uebung3/view/MemberView.java
+++ src/org/hbrs/se/ws20/uebung3/view/MemberView.java
@@ -1,4 +1,17 @@
 package org.hbrs.se.ws20.uebung3.view;
 
+import org.hbrs.se.ws20.uebung3.control.Member;
+import org.hbrs.se.ws20.uebung3.control.Container;
+
+import java.util.List;
+
 public class MemberView {
+
+    // Methode zur Ausgabe der Member des Containers auf Kommandozeile
+    public void dump(List<Member> liste) {
+        for(Member member : liste) {
+            System.out.println(member.toString());
+        }
+    }
+
 }
