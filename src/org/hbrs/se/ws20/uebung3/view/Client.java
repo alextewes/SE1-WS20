f64d4b17cf0544c5914e9585857b4ac261a66c29
Alexander Tewes <alexandertewes7@gmail.com>
2020-11-24 19:03:22 +0100

    Client an Änderungen der MemberImpl angepasst

--- src/org/hbrs/se/ws20/uebung3/view/Client.java
+++ src/org/hbrs/se/ws20/uebung3/view/Client.java
@@ -10,9 +10,9 @@ public class Client {
     public static void main(String[] args) {
         Container container = Container.getInstance();
         MemberView memberView = new MemberView();
-        Member m1 = new MemberImpl();
-        Member m2 = new MemberImpl();
-        Member m3 = new MemberImpl();
+        Member m1 = new MemberImpl(1);
+        Member m2 = new MemberImpl(2);
+        Member m3 = new MemberImpl(3);
         try {
             container.addMember(m1);
             container.addMember(m2);
