935a1d208ccef6b9c74b577a561f6857c82497d7
Alexander Tewes <alexandertewes7@gmail.com>
2020-12-02 13:32:47 +0100

    ContainerException hinzugefügt

---
+++ src/org/hbrs/se/ws20/uebung4/control/container/ContainerException.java
@@ -0,0 +1,9 @@
+package org.hbrs.se.ws20.uebung4.control.container;
+
+public class ContainerException extends Exception {
+
+    public ContainerException(String s) {
+        super(s);
+    }
+
+}
