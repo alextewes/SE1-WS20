5be9507cddae59ed05f23a51b699ea102fe68cae
Alexander Tewes <alexandertewes7@gmail.com>
2020-12-08 20:48:32 +0100

    Funktionalität für Ausgabe der UserStories hinzugefügt

--- src/org/hbrs/se/ws20/uebung4/model/UserStory.java
+++ src/org/hbrs/se/ws20/uebung4/model/UserStory.java
@@ -1,28 +1,27 @@
 package org.hbrs.se.ws20.uebung4.model;
 
 import java.io.Serializable;
+import java.util.Comparator;
 
 public class UserStory implements Serializable {
 
-    private final int ID = UserStoryCount;
-    private static int UserStoryCount;
 
     private String beschreibung;
-    private int mehrwert, strafe, aufwand, risiko;
+    private int ID, mehrwert, strafe, aufwand, risiko;
     private double prio;
 
 
-    public UserStory(String beschreibung, int mehrwert, int strafe, int aufwand, int risiko) {
+    public UserStory(int ID, String beschreibung,  int mehrwert, int strafe, int aufwand, int risiko) {
         if(risiko < 0 || mehrwert < 1) {
             throw new IllegalArgumentException("Ungültige Eingabe!");
         }
-        UserStoryCount++;
+        this.ID = ID;
         this.beschreibung = beschreibung;
         this.mehrwert = mehrwert;
         this.strafe = strafe;
         this.aufwand = aufwand;
         this.risiko = risiko;
-        this.prio = (double)(mehrwert + strafe)/(aufwand + risiko);
+        this.prio = (double)Math.round(((double)(mehrwert + strafe)/(aufwand + risiko))*100)/100;
     }
 
 
@@ -32,11 +31,8 @@ public class UserStory implements Serializable {
 
     @Override
     public String toString() {
-        return super.toString();
-    }
-
-    public static int getUserStoryCount() {
-        return UserStoryCount;
+        return "User Story(ID=" + ID + "): Beschreibung: " + beschreibung + " Mehrwert: "
+                + mehrwert + " Strafe: " + strafe + " Aufwand: " + aufwand + " Risiko: " + risiko + " Prio: " + prio;
     }
 
     public String getBeschreibung() {
@@ -61,33 +57,5 @@ public class UserStory implements Serializable {
 
     public double getPrio() {
         return prio;
-    }
-
-    public static void setUserStoryCount(int userStoryCount) {
-        UserStoryCount = userStoryCount;
-    }
-
-    public void setBeschreibung(String beschreibung) {
-        this.beschreibung = beschreibung;
-    }
-
-    public void setMehrwert(int mehrwert) {
-        this.mehrwert = mehrwert;
-    }
-
-    public void setStrafe(int strafe) {
-        this.strafe = strafe;
-    }
-
-    public void setAufwand(int aufwand) {
-        this.aufwand = aufwand;
-    }
-
-    public void setRisiko(int risiko) {
-        this.risiko = risiko;
-    }
-
-    public void setPrio(double prio) {
-        this.prio = prio;
     }
 }
