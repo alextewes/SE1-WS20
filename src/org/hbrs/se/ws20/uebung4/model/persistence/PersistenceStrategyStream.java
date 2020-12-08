6f240b57688a25259d2ee4e9280dce4d2c0657ca
Alexander Tewes <alexandertewes7@gmail.com>
2020-12-08 20:46:51 +0100

    Problem mit dem Laden von Daten behoben

--- src/org/hbrs/se/ws20/uebung4/model/persistence/PersistenceStrategyStream.java
+++ src/org/hbrs/se/ws20/uebung4/model/persistence/PersistenceStrategyStream.java
@@ -8,17 +8,24 @@ import java.util.List;
 
 public class PersistenceStrategyStream implements PersistenceStrategy<UserStory> {
 
-    ObjectInputStream ois = null;
-    ObjectOutputStream oos = null;
-    FileInputStream fis = null;
-    FileOutputStream fos = null;
-    List<UserStory> newListe =  null;
+    private String LOCATION = "userStories.ser";
+
+    private ObjectOutputStream oos = null;
+    private FileOutputStream fos = null;
+
+    private FileInputStream fis = null;
+    private ObjectInputStream ois = null;
+
+
+    public void setLOCATION(String LOCATION) {
+        this.LOCATION = LOCATION;
+    }
 
     @Override
     public void openConnection() throws PersistenceException {
         try {
-            fis = new FileInputStream("sampleFile");
-            ois = new ObjectInputStream(fis);
+            fos = new FileOutputStream(LOCATION);
+            oos = new ObjectOutputStream(fos);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -27,10 +34,13 @@ public class PersistenceStrategyStream implements PersistenceStrategy<UserStory>
     @Override
     public void closeConnection() throws PersistenceException {
         try {
-            fis.close();
-            fos.close();
-            ois.close();
-            oos.close();
+            // Closing the outputstreams for storing
+            if (oos != null) oos.close();
+            if (fos != null) fos.close();
+
+            // Closing the inputstreams for loading
+            if (ois != null) ois.close();
+            if (fis != null) fis.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -41,14 +51,16 @@ public class PersistenceStrategyStream implements PersistenceStrategy<UserStory>
      * Method for saving a list of Member-objects to a disk (HDD)
      */
     public void save(List<UserStory> list) throws PersistenceException {
+        this.openConnection();
+
         try {
-            openConnection();
-            fos = new FileOutputStream("sampleFile");
-            oos = new ObjectOutputStream(fos);
-            oos.writeObject(list);
-            closeConnection();
+            System.out.println(  list.size() + " User Stories wurden erfolgreich gespeichert!");
+            oos.writeObject( list );
         } catch (IOException e) {
             e.printStackTrace();
+            throw new PersistenceException(PersistenceException.ExceptionType.LoadFailure, "Fehler beim Laden");
+        } finally {
+            this.closeConnection();
         }
     }
 
@@ -56,20 +68,26 @@ public class PersistenceStrategyStream implements PersistenceStrategy<UserStory>
     /**
      * Method for loading a list of Member-objects from a disk (HDD)
      * Some coding examples come for free :-)
-     * Thanks! ;)
      */
     public List<UserStory> load() throws PersistenceException {
-        openConnection();
+
+        List<UserStory> list =  null;
+
         try {
+            fis = new FileInputStream( LOCATION );
+            ois = new ObjectInputStream( fis );
+
             Object obj = ois.readObject();
             if (obj instanceof List<?>) {
-                newListe = (List) obj;
-                return newListe;
+                list = (List) obj;
             }
-        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("LOG: Es wurden " + list.size() + " User Stories erfolgreich reingeladen!");
+            return list;
+        }  catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
+            throw new PersistenceException(PersistenceException.ExceptionType.LoadFailure, "Fehler beim Laden");
+        } finally {
+            this.closeConnection();
         }
-        closeConnection();
-        return null;
     }
 }
