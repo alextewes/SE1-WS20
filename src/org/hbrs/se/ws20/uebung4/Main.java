3b1dd9fb21b1e916d2adda84e670d0bf819ef72a
Alexander Tewes <alexandertewes7@gmail.com>
2020-12-08 22:01:12 +0100

    Main Klasse hinzugefügt

---
+++ src/org/hbrs/se/ws20/uebung4/Main.java
@@ -0,0 +1,12 @@
+package org.hbrs.se.ws20.uebung4;
+
+import org.hbrs.se.ws20.uebung4.control.EingabeDialog;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EingabeDialog eingabeDialog = new EingabeDialog();
+        eingabeDialog.startEingabe();
+    }
+
+}
